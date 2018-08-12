789809656b4415bfb418eee2bafe5db415bfb151
danielleesg <40130817+danielleesg@users.noreply.github.com>
2018-08-12 14:38:17 +1200

    updated version.

--- lib100/src/main/java/com/example/lib100/MyClass.java
+++ lib100/src/main/java/com/example/lib100/MyClass.java
@@ -2,16 +2,25 @@ package com.example.lib100;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyClass {
 
     public static void main (String[] args){
-        String hostname="milou.local";
-        int port=5000;
+        String hostname="fierce-forest-59715.herokuapp.com";
+        int port=80;
 
         try {
             Socket s = new Socket(hostname, port);
+            s.setSoTimeout(1000);
 
+            String request = "GET / HTTP/1.1\r\nHost: fierce-forest-59715.herokuapp.com\r\n\r\n";
+            byte[] requestBytes = request.getBytes();
+            s.getOutputStream().write(requestBytes);
+            Scanner sc = new Scanner(s.getInputStream());
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
             s.close();
         }catch(IOException e){
             e.printStackTrace();
@@ -19,10 +28,8 @@ public class MyClass {
 
         String request = "GET / HTTP/1.1\n\n";
         byte[] requestBytes = request.getBytes();
-        s.getOutputStream().write(requestBytes);
-        //  Scanner sc = new Scanner(s.getInputStream());
-        //while(sc.hasNextLine()){
-            //System.out.println(sc.nextLine());
+
+
     }
 
 
